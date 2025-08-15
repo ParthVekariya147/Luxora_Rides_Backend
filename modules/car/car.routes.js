ce460d2529ae20ee5e71162605534f8ee17193a1
ParthVekariya147 <parthvekariya147@gmail.com>
2025-08-15 17:19:51 +0530

    add cars

--- modules/car/car.routes.js
+++ modules/car/car.routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+    const express = require('express');
 const router = express.Router();
 const carController = require('./car.controller');
 const { authMiddleware } = require('../../middlewares/auth.middleware');

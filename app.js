ecfdd47b33b8a8d98a5a38a63798e0d514d990f3
krishajetani812 <krishajetani0@gmail.com>
2025-08-20 09:39:13 +0530

    chnage localhost

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3001' || 'http://localhost:3002',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3001' || 'http://localhost:3002'|| 'http://localhost:3003',
   credentials: true
 }));
 
